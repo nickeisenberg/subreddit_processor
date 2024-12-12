65f6d1b7d9bfdf5fd710d97bd39a73b3433bd189
nickeisenberg <nickeisenberg@gmail.com>
2024-12-12 11:09:09 -0800

    some helper func

--- eda.py
+++ eda.py
@@ -1,4 +1,5 @@
 import os
+from typing import Literal
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,31 +10,41 @@ from src.crypto_daily_summarizer import (
     get_ticker_counts_from_summarization
 )
 
-summaries = sorted([os.path.join("data", x) for x in os.listdir("./data") if x.endswith(".csv")])
+from sklearn.preprocessing import MinMaxScaler
 
-sentiments = []
-for summary in tqdm(summaries):
-    summary_df = pd.read_csv(
-        summary, index_col=0, na_values=[], keep_default_na=False
+
+
+def get_sentiment_for_ticker(ticker, 
+                             which: Literal["positive", "negative"] = "positive", 
+                             path="./data"):
+    sentiments = []
+    summaries = sorted(
+        [os.path.join(path, x) for x in os.listdir(path) if x.endswith(".csv")]
     )
-    try:
-        sent = get_ticker_sentiment(summary_df)
-    except:
-        continue
-    try:
-        pos = sent.loc["btc"]["positive"]
-    except:
-        pos = 0
-    try:
-        neg = sent.loc["btc"]["negative"]
-    except:
-        neg = 0
-    sentiments.append(pos - neg)
+    for summary in tqdm(summaries):
+        summary_df = pd.read_csv(
+            summary, index_col=0, na_values=[], keep_default_na=False
+        )
+        try:
+            sent = get_ticker_sentiment(summary_df)
+        except:
+            continue
+        try:
+            pos = sent.loc[ticker][which]
+        except:
+            pos = 0
+        sentiments.append(pos)
 
-sentiments = np.array(sentiments)
-sentiments[np.isnan(sentiments)] = 0
+    sentiments = np.array(sentiments)
 
-np.mean(sentiments)
+    sentiments[np.isnan(sentiments)] = 0
 
-plt.plot(sentiments)
+    return sentiments
+
+btc_pos = get_sentiment_for_ticker("btc")
+
+
+plt.plot(
+    MinMaxScaler().fit_transform(btc_pos.reshape((-1, 1))).reshape(-1)
+)
 plt.show()
