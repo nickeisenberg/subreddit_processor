27df3c556c451da41e50bd9f99ed4331f5bfef61
nickeisenberg <nickeisenberg@gmail.com>
2024-12-12 07:33:29 -0800

    btc sentiment for hte past 6 years

--- eda.py
+++ eda.py
@@ -1,22 +1,39 @@
 import os
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from src.crypto_daily_summarizer import get_overal_sentiment_from_summarization
+from tqdm import tqdm
+from src.crypto_daily_summarizer import (
+    get_overal_sentiment_from_summarization,
+    get_ticker_sentiment,
+    get_ticker_counts_from_summarization
+)
 
 summaries = sorted([os.path.join("data", x) for x in os.listdir("./data") if x.endswith(".csv")])
 
 sentiments = []
-for summary in summaries:
-    sent = get_overal_sentiment_from_summarization(pd.read_csv(summary, index_col=0))
+for summary in tqdm(summaries):
+    summary_df = pd.read_csv(
+        summary, index_col=0, na_values=[], keep_default_na=False
+    )
     try:
-        pos = sent["positive"]
+        sent = get_ticker_sentiment(summary_df)
+    except:
+        continue
+    try:
+        pos = sent.loc["btc"]["positive"]
     except:
         pos = 0
     try:
-        neg = sent["negative"]
+        neg = sent.loc["btc"]["negative"]
     except:
         neg = 0
     sentiments.append(pos - neg)
+
+sentiments = np.array(sentiments)
+sentiments[np.isnan(sentiments)] = 0
+
+np.mean(sentiments)
 
 plt.plot(sentiments)
 plt.show()
