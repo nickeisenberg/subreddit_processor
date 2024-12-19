5bb25d755afb7597eae763c210a50c7385c9c57f
nickeisenberg <nickeisenberg@gmail.com>
2024-12-19 10:33:22 -0800

    removed lines from eda script

--- misc/eda.py
+++ misc/eda.py
@@ -3,22 +3,6 @@ from src.summarize.eda_tools import (
     plot_sentiment_and_close,
 )
 
-from src.praw_tools import get_comments_from_submission_id, get_reddit_client
-
-import os
-
-submission_id = "ayxcwl"
-reddit = get_reddit_client(
-    client_id=os.environ["PRAW_CLIENT_ID"],
-    client_secret=os.environ["PRAW_CLIENT_SECRET"],
-    user_agent=os.environ["PRAW_USER_AGENT"]
-)
-comments = get_comments_from_submission_id(reddit, submission_id)
-
-for com in comments:
-    if com.id == "ei67smm":
-        print(com.body)
-
 df = get_all_csv()
 ticker = "ada"
 plot_sentiment_and_close(df, ticker)
