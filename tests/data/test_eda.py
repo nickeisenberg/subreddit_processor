4987873b5f291cff99e3a0b276aec10b144013d0
nickeisenberg <nickeisenberg@gmail.com>
2025-01-05 13:15:48 -0800

    little test for the close and sentiment plotter

--- tests/data/test_eda.py
+++ tests/data/test_eda.py
@@ -3,10 +3,19 @@ import os
 import src.data.eda as eda
 
 
-root = os.path.join(
-    os.getcwd(),
-    'database/crypto/daily_discussions/individual/sentiment_twitter'
-)
-all = eda.make_all_csv(root).dropna()
+def get_all_for_tests():
+    root = os.path.join(
+        os.getcwd(),
+        'database/crypto/daily_discussions/individual/sentiment_twitter'
+    )
+    all = eda.make_all_csv(root).dropna()
+    return all
 
-eda.plot_sentiment_and_close(all, "algo")
+all = get_all_for_tests()
+
+
+def test_plot_sentiment_and_close(ticker:str):
+    global all
+    eda.plot_sentiment_and_close(all, ticker)
+
+test_plot_sentiment_and_close("ada")
