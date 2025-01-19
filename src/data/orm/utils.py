62707a206ef89e98fa09cd1ba8c2ed778ec45bf0
nickeisenberg <nickeisenberg@gmail.com>
2025-01-19 14:59:15 -0800

    added a util to get missing days from the database

--- src/data/orm/utils.py
+++ src/data/orm/utils.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from typing import Any
 from sqlalchemy import text
 from sqlalchemy.engine.base import Engine
@@ -28,3 +29,23 @@ def check_if_primary_key_exists_in_db_query(table_name: str,
         else:
             query += f"{primary_key} = {value} and "
     return query[:-4] + ";"
+
+
+def get_dates(engine: Engine, table_name: str):
+    with engine.connect() as conn:
+        result = conn.execute(text(f"select distinct(date) from {table_name}"))
+    return sorted([x[0] for x in result.fetchall()])
+
+
+def missing_days(engine: Engine, table_name: str, include_today: bool = False):
+    dts = [dt.datetime.strptime(x, "%Y-%m-%d") for x in get_dates(engine, table_name)]
+    today = dt.datetime.strptime(dt.datetime.today().strftime("%Y-%m-%d"), "%Y-%m-%d")
+    dates = []
+    last = dts[-1]
+    while last < today:
+        last += dt.timedelta(days=1)
+        dates.append(last.strftime("%Y-%m-%d"))
+    if include_today:
+        return dates
+    else:
+        return dates[:-1]
